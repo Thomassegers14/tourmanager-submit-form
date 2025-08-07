6fc17e3853d14a99991a14ab57ddef903dd935df
Thomassegers14 <segers.thomas@telenet.be>
2025-08-07 12:16:45 +0200

    add env pw from render

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,15 @@ import cors from 'cors';
 
 dotenv.config();
 
+const pool = new Pool({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
+  ssl: { rejectUnauthorized: false } // Render vereist SSL
+})
+
 const app = express();
 app.use(cors());
 app.use(express.json());
