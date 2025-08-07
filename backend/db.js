f9c9d47b8b9d0fb9cb78203a3faef817d746bba5
Thomassegers14 <segers.thomas@telenet.be>
2025-08-07 13:09:28 +0200

    lets try again

--- backend/db.js
+++ backend/db.js
@@ -6,10 +6,9 @@ const pool = new Pool({
   user: process.env.DB_USER,
   host: process.env.DB_HOST,
   database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  password: process.env.ADMIN_PASSWORD,
+  password: process.env.DB_PASSWORD, // ← alleen deze
   port: process.env.DB_PORT,
   ssl: { rejectUnauthorized: false } // Render vereist SSL
-})
+});
 
 export default pool;
