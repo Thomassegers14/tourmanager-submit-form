2a90e0d5cc8a8c64f7a0697e1d2c96207f6285e6
Thomassegers14 <segers.thomas@telenet.be>
2025-08-07 12:34:08 +0200

    remove pool

--- backend/server.js
+++ backend/server.js
@@ -5,15 +5,6 @@ import cors from 'cors';
 
 dotenv.config();
 
-const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
-  ssl: { rejectUnauthorized: false } // Render vereist SSL
-})
-
 const app = express();
 app.use(cors());
 app.use(express.json());
