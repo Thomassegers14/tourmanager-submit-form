f536bea6e1d422167881484e0271b00e492a3db2
Thomassegers14 <segers.thomas@telenet.be>
2025-08-20 11:02:08 +0200

    still not working

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -3,11 +3,11 @@
     <Eclipse class="icon" />
     <span>darkmode</span>
   </button>
-  <Navbar />
+  <navbar />
   <router-view />
 </template>
 
 <script setup>
 import { Eclipse } from 'lucide-vue-next'
-import Navbar from './components/NavBar.vue';
+import navbar from './components/NavBar.vue';
 </script>
