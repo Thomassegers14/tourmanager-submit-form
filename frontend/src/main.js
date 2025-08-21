cd7c9e33832315dd1f97cb4535555248bfbd619f
Thomassegers14 <segers.thomas@telenet.be>
2025-08-21 13:43:19 +0200

    remove darkmode code

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -6,21 +6,3 @@ import router from './router'
 const app = createApp(App)
 app.use(router)
 app.mount('#app')
-
-
-document.addEventListener('DOMContentLoaded', () => {
-  const toggleButton = document.getElementById('toggle-darkmode');
-
-  const prefersDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
-  const saved = localStorage.getItem('darkmode');
-
-  if (saved === 'true' || (saved === null && prefersDark)) {
-    document.body.classList.add('darkmode');
-  }
-
-  toggleButton.addEventListener('click', () => {
-    document.body.classList.toggle('darkmode');
-    const isDark = document.body.classList.contains('darkmode');
-    localStorage.setItem('darkmode', isDark);
-  });
-});
