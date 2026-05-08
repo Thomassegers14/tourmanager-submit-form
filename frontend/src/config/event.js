99682b05ae925fba4dafb2058eb2a82bfe958da0
Thomassegers14 <segers.thomas@telenet.be>
2026-05-08 10:55:01 +0200

    update EVENT_DEADLINE to reflect correct submission time

--- frontend/src/config/event.js
+++ frontend/src/config/event.js
@@ -1,4 +1,4 @@
 export const EVENT_ID = 'giro-d-italia'
 export const EVENT_YEAR = 2026
 export const EVENT_NAME = "Giro d'Italia"
-export const EVENT_DEADLINE = new Date('2026-05-08T10:00:00Z')
+export const EVENT_DEADLINE = new Date('2026-05-08T13:00:00Z')
